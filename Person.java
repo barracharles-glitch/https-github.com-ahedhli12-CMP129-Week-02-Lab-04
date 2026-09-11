5022fe72c9b99314e1fb24c478eb8877fc171a96
barracharles-glitch <barra.charles@student.ccm.edu>
2026-09-11 14:29:24 -0400

    9/11/2026

--- Person.java
+++ Person.java
@@ -1,3 +1,40 @@
 public class Person {
-    
+    private String name, email;
+    private int age;
+
+    Person(String n, int a, String e){
+        name = n;
+        age = a;
+        email = e;
+    }
+
+    Person(){
+    }
+
+    public void setName(String n){
+        name = n;
+    }
+    public void setAge(int a){
+        age = a;
+    }
+    public void setEmail(String e){
+        email = e;
+    }
+
+    public String getName(){
+        return name;
+    }
+    public int getAge(){
+        return age;
+
+    }
+    public String getEmail(){
+        return email;
+    }
+
+    public void displayInfo(){
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + name);
+        System.out.println("Email: " + name);
+    }
 }
--- PersonTest.java
+++ PersonTest.java
@@ -1,3 +1,19 @@
 public class PersonTest {
-    
+    public static void main(String args []){
+
+        Person person1 = new Person();
+        Person person2 = new Person();
+
+        person1.setName("Charlie");
+        person2.setName("Anthony");
+        person1.setAge(17);
+        person2.setAge(16);
+        person1.setEmail("barra.charles@student.ccm.edu");
+        person2.setEmail("guttridge.anthony@student.ccm.edu");
+        
+        System.out.println("Person 1 Info: ");
+        person1.displayInfo();
+        System.out.println("Person 2 Info: ");
+        person2.displayInfo();
+    }
 }
